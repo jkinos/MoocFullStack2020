14c9c5310a72ac6c6b7a78f9f66815d44962b0e0
JennaKinos <jenna.kinos@gmail.com>
2020-05-13 13:18:28 +0300

    describe for the first test for Blog -component

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,36 +4,37 @@ import { render } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
+describe('<Blog/>', ()=> {
+    beforeEach
+    test('renders title and author', () => {
+        const user = {id: 121, username: 'abc', name: 'abc defg'}
 
-test('renders title and author', () => {
-    const user= {id:121, username:'abc', name:'abc defg'}
+        const blog = {
+            user: 123,
+            title: 'Component testing is done with react-testing-library',
+            author: 'react developer',
+            likes: 3,
+            url: 'https://somewebsite.com'
+        }
 
-    const blog = {
-        user: 123,
-        title: 'Component testing is done with react-testing-library',
-        author: 'react developer',
-        likes: 3,
-        url: 'https://somewebsite.com'
-    }
-
-    const component = render(
-        <Blog blog={blog} user={user}/>
-    )
-    const element = component.getByText(
-        'https://somewebsite.com'
-    )
-
-    component.debug()
-    console.log(prettyDOM(element))
+        const component = render(
+            <Blog blog={blog} user={user}/>
+        )
+        const element = component.container.querySelector('.showOrHide')
 
 
-    expect(component.container).toHaveTextContent(
-        'Component testing is done with react-testing-library'
-    )
-    expect(component.container).toHaveTextContent('react developer')
+        component.debug()
+        console.log(prettyDOM(element))
 
-    expect(element).toHaveStyle('display:none')
 
+        expect(component.container).toHaveTextContent(
+            'Component testing is done with react-testing-library'
+        )
+        expect(component.container).toHaveTextContent('react developer')
+
+        expect(element).toHaveStyle('display:none')
+
+    })
 })
 
 
