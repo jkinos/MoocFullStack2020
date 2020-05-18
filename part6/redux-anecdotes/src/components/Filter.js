a823333a632497033198242fe8cec1e8a0d8d084
JennaKinos <jenna.kinos@gmail.com>
2020-05-18 19:34:07 +0300

    6b done

--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useDispatch } from 'react-redux'
-import filterAnecdotes from '../reducers/filterReducer'
+import { filterAnecdotes } from '../reducers/filterReducer'
 
 
 const Filter = () => {
