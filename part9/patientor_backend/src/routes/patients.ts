1178afa739edffc54d24d9d6e17ceed3ee0cd20f
JennaKinos <jenna.kinos@gmail.com>
2020-06-21 21:45:44 +0300

    9.19 done

--- part9/patientor_backend/src/routes/patients.ts
+++ part9/patientor_backend/src/routes/patients.ts
@@ -13,7 +13,7 @@ router.get('/:id', (req, res) => {
     const patient = patientService.findById(req.params.id);
 
     if (patient) {
-        res.send(patient);
+        res.send([patient]);
     } else {
         res.sendStatus(404);
     }
